2e1911517a5b289f3a721bff8de68f589177d4cd
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-03 11:35:34 +0100

    Exercise_6

--- Level_1/__init__.py
+++ Level_1/__init__.py
@@ -131,5 +131,25 @@ x = Class_1("Salom, Doston")
 x.printString()
 
 
+# =========================   Question_6: ===========================================
+# Write a program which takes 2 digits, X,Y as input and generates
+# a 2-dimensional array.
+# The element value in the i-th row and j-th column of the array should be i*j.
+# Note: i=0,1.., X-1; j=0,1,¡­Y-1.
+# Example
+# Suppose the following inputs are given to the program:
+# 3,5
+# Then, the output of the program should be:
+# [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
 
 
+def two_dimens_arr(x, y):
+    l1 = []
+    for a in range(x):
+        l1.append([])
+        for b in range(y):
+            d = a * b
+            l1[a].append(a*b)
+    print(l1)
+    return
+two_dimens_arr(4, 4)
