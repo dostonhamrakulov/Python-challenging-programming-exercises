30f755f5af64c9bd4928985b0c10ece10fbf7f8c
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2019-03-04 18:12:47 +0100

    Descripion_9

--- Solution.py
+++ Solution.py
@@ -179,7 +179,7 @@ def prog_3():
 
 
 
-# =========================   Question_7: ===========================================
+# =========================   Question_8: ===========================================
 # Write a program that accepts a sequence of whitespace separated words as input and prints the words
 # after removing all duplicate words and sorting them alphanumerically.
 # Suppose the following input is supplied to the program:
